73ae5e55b587c43e0b0a0502c6ca4d3d32e1da54
Federico H <fede@federicos-air.lan>
2025-08-14 22:41:32 -0400

    Added workable edit, delete and Mark complete buttons

--- habit-tracker/C11/Fede1220/src/main.js
+++ habit-tracker/C11/Fede1220/src/main.js
@@ -7,7 +7,8 @@ form.addEventListener('submit', (event) => {
 
     const habit = {
         name: data.get("habit_name"),
-        targetStreak: Number(data.get("target_streak"))
+        targetStreak: Number(data.get("target_streak")),
+        completions: []
     }
 
     habits.push(habit)
@@ -20,15 +21,54 @@ form.addEventListener('submit', (event) => {
 const renderHabits = (habits) => {
     const habitList = document.getElementById('habit_list')
 
-    // for (let i=0; i < habits.length; i++) {
-    //     const habit = habits[i]
+    //while loop resets array everytime 'renderHabits' is called to allow deletion. 
+    while (habitList.firstChild){
+        habitList.removeChild(habitList.firstChild)
+    }
+    
+    habits.forEach((habit, index) => {
+        const li = document.createElement('li')
+        li.textContent = `Habit Name: ${habit.name} --- Target Streak: ${habit.targetStreak} days`
+            
+        const deleteBtn = document.createElement('button')  //Creates a delete button for each habit.
+        deleteBtn.textContent = 'Delete'
 
-    //     const li = document.createElement('li')
-    //     li.textContent = `Habit Name: ${habit.name} - Target Streak: ${habit.targetStreak} days`
-    //     habitList.appendChild(li)
-    // }
+        deleteBtn.addEventListener('click', () => {
+            habits.splice(index, 1)
+            renderHabits(habits)
+        })
 
-    habitList.innerHTML = habits.map(habit => {
-                return `<li>${habit.name}; ${habit.targetStreak}</li>`
-            }).join(`\n`)
+        const completeBtn = document.createElement('button')    //Creates completion button for each habit.
+        completeBtn.textContent = 'Mark Complete'
+        
+        completeBtn.addEventListener('click', () => {
+            const todayDate = new Date().toISOString().split('T')[0]    // Gets real current date and stores it into an array. 
+            if (!habit.completions.includes(todayDate)){
+                habit.completions.push(todayDate)
+                alert(`Marked "${habit.name}" completed today.`)
+            }
+            else {
+                alert(`Already Completed.`)
+            }
+            renderHabits(habits)
+        })
+
+        const editBtn = document.createElement('button')
+        editBtn.textContent = 'edit'
+        
+        editBtn.addEventListener('click', () => {
+            const newName = prompt("Enter new habit name: ", habit.name)    // Asks user to type a new habit name.
+            if(newName) habit.name = newName
+
+            const newStreak = prompt("Enter new target Streak : ", habit.targetStreak)
+            if(newStreak) habit.targetStreak = newStreak
+
+            renderHabits(habits)
+        })
+
+        li.appendChild(deleteBtn)        
+        habitList.appendChild(completeBtn)
+        habitList.appendChild(li)
+        habitList.appendChild(editBtn)
+    })
 }
